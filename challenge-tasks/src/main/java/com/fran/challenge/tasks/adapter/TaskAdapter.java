d0395f59b27dbddc3e87f9957ac75da96e36c414
garrido-mosqueira <28862509+garrido-mosqueira@users.noreply.github.com>
2022-05-25 07:35:51 +0200

    Narrowing down the context of the demo

--- challenge-tasks/src/main/java/com/fran/challenge/tasks/adapter/TaskAdapter.java
+++ challenge-tasks/src/main/java/com/fran/challenge/tasks/adapter/TaskAdapter.java
@@ -1,7 +1,5 @@
 package com.fran.challenge.tasks.adapter;
 
-import com.fran.challenge.domain.exceptions.InternalException;
-import com.fran.challenge.domain.exceptions.NotFoundException;
 import com.fran.challenge.domain.model.Task;
 import com.fran.challenge.domain.port.*;
 import com.fran.challenge.persistence.adapter.PersistenceAdapter;
@@ -10,12 +8,9 @@ import com.fran.challenge.tasks.model.Counter;
 import com.fran.challenge.tasks.service.CounterService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.commons.io.IOUtils;
 import org.quartz.SchedulerException;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.net.URL;
 import java.util.List;
 import java.util.Optional;
 
@@ -69,51 +64,14 @@ public class TaskAdapter
 
     @SneakyThrows
     @Override
-    public void executeTask(Task fileTask) {
-        //TODO (fgarrido) find a better way to decide between task
-        if (fileTask.getBegin() != null) {
-            executeCounterTask(fileTask);
-        } else {
-            executeFileTask(fileTask);
-        }
-    }
-
-    public void executeFileTask(Task fileTask) {
-        URL url = Thread.currentThread().getContextClassLoader().getResource("challenge.zip");
-        if (url == null) {
-            throw new InternalException("Zip file not found");
-        }
-        try {
-            storeResult(fileTask, url);
-        } catch (Exception e) {
-            throw new InternalException(e);
-        }
+    public void executeTask(Task task) {
+        executeCounterTask(task);
     }
 
     public void executeCounterTask(Task task) throws SchedulerException {
         Counter counter = mapper.toCounter(task);
         counterService.runCounterJob(counter);
         persistenceAdapter.updateExecution(task);
-    }
-
-    public File getTaskResult(String taskId) {
-        Task task = persistenceAdapter.getTask(taskId).orElseThrow(NotFoundException::new);
-        File inputFile = new File(task.getStorageLocation());
-        if (!inputFile.exists()) {
-            throw new InternalException("File not generated yet");
-        }
-        return inputFile;
-    }
-
-    public void storeResult(Task task, URL url) throws IOException {
-        File outputFile = File.createTempFile(task.getId(), ".zip");
-        outputFile.deleteOnExit();
-        task.setStorageLocation(outputFile.getAbsolutePath());
-        persistenceAdapter.updateTask(task.getId(), task);
-        try (InputStream is = url.openStream();
-             OutputStream os = new FileOutputStream(outputFile)) {
-            IOUtils.copy(is, os);
-        }
     }
 
 }
