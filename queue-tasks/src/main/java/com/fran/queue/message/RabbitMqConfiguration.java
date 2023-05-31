7faa5830abaef44a804984030fdbaf3a9c657c70
Francisco Garrido Mosqueira <f.garrido@celonis.com>
2023-05-31 09:40:22 +0200

    Adding new module for RabbitMq support

---
+++ queue-tasks/src/main/java/com/fran/queue/message/RabbitMqConfiguration.java
@@ -0,0 +1,42 @@
+package com.fran.queue.message;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.rabbitmq.*;
+
+@Configuration
+public class RabbitMqConfiguration {
+
+    @Bean
+    Mono<Connection> connectionMono() {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.useNio();
+        return Mono.fromCallable(() -> connectionFactory.newConnection("sender-sms-rabbitmq"));
+    }
+
+    @Bean
+    public SenderOptions senderOptions(Mono<Connection> connectionMono) {
+        return new SenderOptions().connectionMono(connectionMono)
+                .resourceManagementScheduler(Schedulers.boundedElastic());
+    }
+
+    @Bean
+    public Sender sender(SenderOptions senderOptions) {
+        return RabbitFlux.createSender(senderOptions);
+    }
+
+    @Bean
+    public ReceiverOptions receiverOptions(Mono<Connection> connectionMono) {
+        return new ReceiverOptions().connectionMono(connectionMono);
+    }
+
+    @Bean
+    Receiver receiver(ReceiverOptions receiverOptions) {
+        return RabbitFlux.createReceiver(receiverOptions);
+    }
+
+}
