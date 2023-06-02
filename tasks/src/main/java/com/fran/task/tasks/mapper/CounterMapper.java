5d7293e9f244c3c69bd4f874b9b21e537f43f190
Francisco Garrido Mosqueira <f.garrido@celonis.com>
2023-06-02 17:32:49 +0200

    Adding @Component is redundant since Mapstruct already added it in the Implementation

--- tasks/src/main/java/com/fran/task/tasks/mapper/CounterMapper.java
+++ tasks/src/main/java/com/fran/task/tasks/mapper/CounterMapper.java
@@ -3,11 +3,9 @@ package com.fran.task.tasks.mapper;
 import com.fran.task.domain.model.Task;
 import com.fran.task.tasks.model.Counter;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 @Mapper(componentModel = "spring")
 public interface CounterMapper {
 
