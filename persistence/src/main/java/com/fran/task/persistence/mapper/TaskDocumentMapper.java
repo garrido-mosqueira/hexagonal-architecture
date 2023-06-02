5208cd3e55d882057474d2c4229a09990861645f
garrido-mosqueira <28862509+garrido-mosqueira@users.noreply.github.com>
2023-06-02 17:32:49 +0200

    Adding @Component is redundant since Mapstruct already added it in the Implementation

--- persistence/src/main/java/com/fran/task/persistence/mapper/TaskDocumentMapper.java
+++ persistence/src/main/java/com/fran/task/persistence/mapper/TaskDocumentMapper.java
@@ -3,11 +3,9 @@ package com.fran.task.persistence.mapper;
 import com.fran.task.domain.model.Task;
 import com.fran.task.persistence.entities.TaskDocument;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 @Mapper(componentModel = "spring")
 public interface TaskDocumentMapper {
 
