079485ef7e83f0c77562ad86b28cba7d04ca6a67
garrido-mosqueira <28862509+garrido-mosqueira@users.noreply.github.com>
2023-06-07 23:42:52 +0200

    Innit module to stark working with Virtual Threads

---
+++ virtual-threads-tasks/src/main/java/com/fran/threads/config/TaskConfiguration.java
@@ -0,0 +1,25 @@
+package com.fran.threads.config;
+
+import com.fran.threads.model.TaskVirtualThread;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class TaskConfiguration {
+
+    @Bean
+    public Map<String, TaskVirtualThread> taskRegister() {
+        return new ConcurrentHashMap<>();
+    }
+
+    @Bean
+    public ExecutorService executorService (){
+        return Executors.newCachedThreadPool();
+    }
+
+}
---
+++ virtual-threads-tasks/src/main/java/com/fran/threads/exception/CounterTaskNotFoundException.java
@@ -0,0 +1,6 @@
+package com.fran.threads.exception;
+
+public class CounterTaskNotFoundException extends RuntimeException{
+    public CounterTaskNotFoundException(String s) {
+    }
+}
