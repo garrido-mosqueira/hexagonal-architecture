e7bc035ab7fc9f1dbf0cd922fff43113b9d84ce4
Francisco Garrido Mosqueira <f.garrido@celonis.com>
2023-06-13 12:23:17 +0200

    Include ErrorHandler when Task is not found in Running Register

--- domain/src/main/java/com/fran/task/domain/exceptions/NotFoundException.java
+++ domain/src/main/java/com/fran/task/domain/exceptions/NotFoundException.java
@@ -1,4 +1,12 @@
 package com.fran.task.domain.exceptions;
 
 public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
 }
