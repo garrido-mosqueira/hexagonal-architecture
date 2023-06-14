4385ce6cf55862d867501967ad412b994391c372
Francisco Garrido Mosqueira <f.garrido@celonis.com>
2023-06-14 15:37:32 +0200

    Using the new @AutoConfiguration introduced in SpringBoot 3 to register beans in other modules

--- persistence/src/main/java/com/fran/task/FileDBConfiguration.java
+++ persistence/src/main/java/com/fran/task/FileDBConfiguration.java
@@ -1,10 +1,10 @@
 package com.fran.task;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
+@AutoConfiguration
 @EnableScheduling
 @ComponentScan
 public class FileDBConfiguration {
--- quartz-tasks/src/main/java/com/fran/quartz/TaskConfiguration.java
+++ quartz-tasks/src/main/java/com/fran/quartz/TaskConfiguration.java
@@ -1,9 +1,9 @@
 package com.fran.quartz;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 @ComponentScan
 public class TaskConfiguration {
 }
