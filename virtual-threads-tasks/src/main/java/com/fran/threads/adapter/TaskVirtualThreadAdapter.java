fc0d71f34a366a9b2b97b8b3695a7305527a71ab
san_fran <28862509+garrido-mosqueira@users.noreply.github.com>
2025-10-29 21:27:15 +0100

    Migrate task storage from `ConcurrentHashMap` to Redis for improved scalability and persistence.

--- virtual-threads-tasks/src/main/java/com/fran/threads/adapter/TaskVirtualThreadAdapter.java
+++ virtual-threads-tasks/src/main/java/com/fran/threads/adapter/TaskVirtualThreadAdapter.java
@@ -6,51 +6,71 @@ import com.fran.threads.exception.CounterTaskNotFoundException;
 import com.fran.threads.model.TaskVirtualThread;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TaskVirtualThreadAdapter implements TaskManager {
 
-    private final Map<String, TaskVirtualThread> taskRegister;
+    private final RedisTemplate<String, TaskVirtualThread> tasksRegister;
+    private static final String TASK_REGISTER_PREFIX = "task:register:";
 
     @Override
     public void cancelTask(String taskId) {
-        TaskVirtualThread taskThread = taskRegister.get(taskId);
-        if (taskThread.thread() != null) {
-            taskThread.thread().interrupt();
+        TaskVirtualThread taskThread = getTaskVirtualThread(TASK_REGISTER_PREFIX + taskId);
+        if (taskThread != null && taskThread.task() != null) {
+            tasksRegister.opsForValue().set(
+                    TASK_REGISTER_PREFIX + taskId,
+                    new TaskVirtualThread(taskThread.task(), true)
+            );
         }
     }
 
     @Override
     public Task executeTask(Task task) {
-        Thread.ofVirtual().start(
-                () -> {
-                    taskRegister.put(task.id(), new TaskVirtualThread(task, Thread.currentThread()));
-                    for (int i = task.begin(); i <= task.finish(); i++) {
-                        Task updatedTask = task.withProgress(i);
-                        // Update the task in the register
-                        taskRegister.put(task.id(), new TaskVirtualThread(updatedTask, Thread.currentThread()));
-                        log.info("Counter progress from Virtual Thread is '{}' for '{}'", updatedTask.progress(), updatedTask.id());
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            executor.submit(
+                    () -> {
+                        tasksRegister.opsForValue().set(TASK_REGISTER_PREFIX + task.id(), new TaskVirtualThread(task, false));
+                        int i = task.begin();
+                        TaskVirtualThread taskThread;
+                        do {
+                            Task updatedTaskWithNewProgress = task.withProgress(i);
+                            tasksRegister.opsForValue().set(TASK_REGISTER_PREFIX + task.id(), new TaskVirtualThread(updatedTaskWithNewProgress, false));
+                            log.info("Counter progress from Virtual Thread is '{}' for '{}'", updatedTaskWithNewProgress.progress(), updatedTaskWithNewProgress.id());
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                                break;
+                            }
+                            i++;
+                            taskThread = getTaskVirtualThread(TASK_REGISTER_PREFIX + task.id());
+                        } while (i <= task.finish() && taskThread != null && !taskThread.isCancelled());
+                        tasksRegister.delete(TASK_REGISTER_PREFIX + task.id());
+                        log.info("End counter progress from Virtual Thread for '{}'", task.id());
                     }
-                }
-        );
+            );
+            executor.shutdown();
+        }
 
         return task;
     }
 
     @Override
     public List<Task> getAllRunningCounters() {
-        return taskRegister.values().stream()
+        Set<String> keys = tasksRegister.keys(TASK_REGISTER_PREFIX + "*");
+        return keys.stream()
+                .map(this::getTaskVirtualThread)
+                .filter(Objects::nonNull)
                 .map(TaskVirtualThread::task)
                 .filter(task -> task.finish() > task.progress())
                 .toList();
@@ -58,11 +78,15 @@ public class TaskVirtualThreadAdapter implements TaskManager {
 
     @Override
     public Task getRunningCounter(String counterId) {
-        TaskVirtualThread taskThread = taskRegister.get(counterId);
+        TaskVirtualThread taskThread = getTaskVirtualThread(TASK_REGISTER_PREFIX + counterId);
         if (taskThread == null) {
             throw new CounterTaskNotFoundException("Failed to find counter with ID " + counterId);
         }
         return taskThread.task();
+    }
+
+    private TaskVirtualThread getTaskVirtualThread(String key) {
+        return tasksRegister.opsForValue().get(key);
     }
 
 }
