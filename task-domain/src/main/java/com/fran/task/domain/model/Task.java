84d914eb55fd07cf4bf23255fcddb168ee395281
san_fran <28862509+garrido-mosqueira@users.noreply.github.com>
2025-10-29 13:11:15 +0100

    Remove `storageLocation` references from `Task` and related methods for simplification and cleanup.

--- task-domain/src/main/java/com/fran/task/domain/model/Task.java
+++ task-domain/src/main/java/com/fran/task/domain/model/Task.java
@@ -9,37 +9,36 @@ public record Task(
         LocalDateTime lastExecution,
         Integer begin,
         Integer finish,
-        Integer progress,
-        String storageLocation) {
+        Integer progress) {
 
     public Task withProgress(Integer newProgress) {
         return new Task(id, name, creationDate, lastExecution,
-                begin, finish, newProgress, storageLocation);
+                begin, finish, newProgress);
     }
 
     public Task withCreationDate(LocalDateTime newCreationDate) {
         return new Task(id, name, newCreationDate, lastExecution,
-                begin, finish, progress, storageLocation);
+                begin, finish, progress);
     }
 
     public Task withLastExecution(LocalDateTime newLastExecution) {
         return new Task(id, name, creationDate, newLastExecution,
-                begin, finish, progress, storageLocation);
+                begin, finish, progress);
     }
 
-    public Task withName(String newName){
+    public Task withName(String newName) {
         return new Task(id, newName, creationDate, lastExecution,
-                begin, finish, progress, storageLocation);
+                begin, finish, progress);
     }
 
     public Task withBegin(Integer newBegin) {
         return new Task(id, name, creationDate, lastExecution,
-                newBegin, finish, progress, storageLocation);
+                newBegin, finish, progress);
     }
 
     public Task withFinish(Integer newFinish) {
         return new Task(id, name, creationDate, lastExecution,
-                begin, newFinish, progress, storageLocation);
+                begin, newFinish, progress);
     }
 
 }
