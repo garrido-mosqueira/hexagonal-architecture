3bc5ae82350ca5998b70c98f600036e8e8e0feee
san_fran <28862509+garrido-mosqueira@users.noreply.github.com>
2025-11-04 18:09:45 +0100

    Add `taskType` field to `Task`, `TaskCounter`, and `TaskDocument` for task categorization

--- task-domain/src/main/java/com/fran/task/domain/model/Task.java
+++ task-domain/src/main/java/com/fran/task/domain/model/Task.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record Task(
         String id,
         String name,
+        TaskType taskType,
         LocalDateTime creationDate,
         LocalDateTime lastExecution,
         Integer begin,
@@ -12,32 +13,32 @@ public record Task(
         Integer progress) {
 
     public Task withProgress(Integer newProgress) {
-        return new Task(id, name, creationDate, lastExecution,
+        return new Task(id, name, taskType, creationDate, lastExecution,
                 begin, finish, newProgress);
     }
 
     public Task withCreationDate(LocalDateTime newCreationDate) {
-        return new Task(id, name, newCreationDate, lastExecution,
+        return new Task(id, name, taskType, newCreationDate, lastExecution,
                 begin, finish, progress);
     }
 
     public Task withLastExecution(LocalDateTime newLastExecution) {
-        return new Task(id, name, creationDate, newLastExecution,
+        return new Task(id, name, taskType, creationDate, newLastExecution,
                 begin, finish, progress);
     }
 
     public Task withName(String newName) {
-        return new Task(id, newName, creationDate, lastExecution,
+        return new Task(id, newName, taskType, creationDate, lastExecution,
                 begin, finish, progress);
     }
 
     public Task withBegin(Integer newBegin) {
-        return new Task(id, name, creationDate, lastExecution,
+        return new Task(id, name, taskType, creationDate, lastExecution,
                 newBegin, finish, progress);
     }
 
     public Task withFinish(Integer newFinish) {
-        return new Task(id, name, creationDate, lastExecution,
+        return new Task(id, name, taskType, creationDate, lastExecution,
                 begin, newFinish, progress);
     }
 
